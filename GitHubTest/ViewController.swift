4b331ccef9a67d0104bf4be5d17345e6fcf04846
Alexander <algashev@mail.ru>
2019-11-19 20:40:46 +0300

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Александр Алгашев on 19.11.2019.
+//  Copyright © 2019 Александр Алгашев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
